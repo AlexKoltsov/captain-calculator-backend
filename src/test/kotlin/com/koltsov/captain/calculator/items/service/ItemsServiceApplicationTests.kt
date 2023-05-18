555db2a998147bd73c58fe8969bb5899485091f8
Alexander Koltsov <a.koltsov@dats.team>
2023-05-18 15:08:49 +0300

    initial

---
+++ src/test/kotlin/com/koltsov/captain/calculator/items/service/ItemsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.koltsov.captain.calculator.items.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItemsServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
